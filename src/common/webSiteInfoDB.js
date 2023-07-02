c3a899d3dcc3e3902a80aee6d557a5cbe8ca5c77
Ke Qi <shellaqi010@gmail.com>
2023-07-02 12:58:11 -0400

    about us component

--- src/common/webSiteInfoDB.js
+++ src/common/webSiteInfoDB.js
@@ -58,4 +58,48 @@ export const webSiteInfoDB = {
       type: "shoes",
     },
   ],
+  aboutUs: [
+    {
+      title: "We are miralou",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here",
+        image: "/AdvancedReact_project/images/about-2.png",
+        type: "shoes",            
+    },
+    {
+      title: "Shops worldwide",
+      description:
+        "Orci fusce libero duis nunc nam aliquet suspendisse enim cras. imperdiet turpis ut nulla sapien.",
+        image: "/AdvancedReact_project/images/about-2.png",
+        type: "shoes",            
+    }, 
+    {
+      title: "24/7 support",
+      description:
+        "Orci fusce libero duis nunc nam aliquet suspendisse enim cras. imperdiet turpis ut nulla sapien.",
+        image: "/AdvancedReact_project/images/about-2.png",
+        type: "shoes",            
+    }, 
+    {
+      title: "Big saving shop",
+      description:
+        "Orci fusce libero duis nunc nam aliquet suspendisse enim cras. imperdiet turpis ut nulla sapien.",
+        image: "/AdvancedReact_project/about-2.png",
+        type: "shoes",            
+    },            
+    {
+      title: "Shop by categories",
+      description:
+        "Pick a category you need",
+        image: ["AdvancedReact_project/images/women.png","AdvancedReact_project/images/men.png","AdvancedReact_project/images/kids.png"],
+        type: "shoes",        
+    },
+    {
+      title: "Our team",
+      description:
+        "Professionals that work with us",
+      image: "../images/main-slider-shoe.png",
+      type: "shoes",
+    },
+  ],  
 };
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { webSiteInfoDB } from "../common/webSiteInfoDB.js";
+import './styles/about.scss';
+import './styles/home.scss';
 
 const AboutUs = () => {
-  return <div>About  Us</div>;
+
+  const { headerNavigation,aboutUs } = webSiteInfoDB;
+
+  return (
+    <section className="about">
+      <div className="hero">
+        <div className="container">
+       <p className="title">{ headerNavigation[0].title }</p>
+        </div>
+      </div>
+      <div className="container2">
+          <div className="wr">
+            <p className="title">{aboutUs[0].title}</p>
+            <span className="desc">{aboutUs[0].description}</span>
+            </div>
+            <img className='wrg' src={aboutUs[0].image}/>
+      </div>
+      <div className="scrp">
+        <div className="p1">
+              <p className="title">{aboutUs[1].title}</p>
+              <p className="desc">{aboutUs[1].description}</p>
+        </div>
+        <p className="divider"></p>
+        <div className="p2">
+              <p className="title">{aboutUs[2].title}</p>
+              <p className="desc">{aboutUs[2].description}</p>
+        </div>
+        <p className="divider"></p>
+        <div className="p3">
+              <p className="title">{aboutUs[3].title}</p>
+              <span className="desc">{aboutUs[3].description}</span>
+        </div>                            
+      </div>
+      <section className="categ">
+        <div className="container3">
+          <p className="title">{aboutUs[4].title}</p>
+          <span className="desc">{aboutUs[4].description}</span>
+
+          {/* <div className="cateImg">
+            <img src={aboutUs[4].image[0]}/>
+            <img src={aboutUs[4].image[1]}/>
+            <img src={aboutUs[4].image[2]}/>
+          </div> */}
+        </div> 
+        
+      </section>
+      <section className="prodacts">
+        <div className="container">
+          <h2 className="products-title"></h2>
+          <div className="wrapper">
+
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default AboutUs;
