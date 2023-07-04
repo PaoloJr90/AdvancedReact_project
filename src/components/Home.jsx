d5c07dd5a8e94ce7f3b9a901aad40a2a675ddc1b
Yevhen Khreptun <khreptun.e@gmail.com>
2023-07-04 18:16:15 -0400

    Created a Gallery component for the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import MainSlider from "./MainSlider";
 import SmallProductCard from "./SmallProductCard";
 import ProductContext from "./ProductContext";
 import ReviewsSlider from "./ReviewsSlider";
+import Gallery from "./Gallery";
 import { Link } from "react-router-dom";
 
 import { webSiteInfoDB } from "../common/webSiteInfoDB.js";
@@ -17,52 +18,55 @@ const Home = () => {
   const { men } = productsInfo;
 
   return (
-    <section className="home">
-      <section className="hero">
-        <div className="container">
-          <MainSlider slides={mainSlider} />
-        </div>
-      </section>
-      <Categories />
-      <section className="prodacts">
-        <div className="container">
-          <h2 className="products-title">Our products</h2>
-          <div className="wrapper">
-            {men.map((product, index) => {
-              const { name, model, image, price, id } = product;
-
-              if (index > 7) {
-                return;
-              }
-
-              return (
-                <SmallProductCard
-                  key={`product-card-${index}${id}`}
-                  productId={id}
-                  productName={name}
-                  productModel={model}
-                  productPrice={price}
-                  productImage={image}
-                />
-              );
-            })}
+    <>
+      <section className="home">
+        <section className="hero">
+          <div className="container">
+            <MainSlider slides={mainSlider} />
           </div>
-          <Link className="btn" to={"/shop"}>
-            Show more
-          </Link>
-          <div className="about-us">
-            <div className="images-block">
-              <img src={homePage.image} alt={homePage.title} />
+        </section>
+        <Categories />
+        <section className="prodacts">
+          <div className="container">
+            <h2 className="products-title">Our products</h2>
+            <div className="wrapper">
+              {men.map((product, index) => {
+                const { name, model, image, price, id } = product;
+
+                if (index > 7) {
+                  return;
+                }
+
+                return (
+                  <SmallProductCard
+                    key={`product-card-${index}${id}`}
+                    productId={id}
+                    productName={name}
+                    productModel={model}
+                    productPrice={price}
+                    productImage={image}
+                  />
+                );
+              })}
             </div>
-            <div className="about-us-description">
-              <h2>{homePage.title}</h2>
-              <p>{homePage.description}</p>
+            <Link className="btn" to={"/shop"}>
+              Show more
+            </Link>
+            <div className="about-us">
+              <div className="images-block">
+                <img src={homePage.image} alt={homePage.title} />
+              </div>
+              <div className="about-us-description">
+                <h2>{homePage.title}</h2>
+                <p>{homePage.description}</p>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+        <ReviewsSlider slides={reviewsSlider} />
       </section>
-      <ReviewsSlider slides={reviewsSlider} />
-    </section>
+      <Gallery />
+    </>
   );
 };
 
