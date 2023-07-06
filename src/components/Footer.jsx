eab34484bcfb043b15f90ee827027182961d0f22
PJ Angeloni <pj.angeloni@gmail.com>
2023-07-06 15:13:31 -0400

    more work done on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,33 @@ import { webSiteInfoDB } from "../common/webSiteInfoDB";
 import NavItem from "./NavItem";
 import "./styles/footer.scss";
 import { Link } from "react-router-dom";
+import { ImFacebook } from "react-icons/im";
+import { BsInstagram, BsVimeo, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   const { footerNavigation, footerNavigation1 } = webSiteInfoDB;
   return (
-    <footer className="container">
+    <footer className="footer">
       <Link to="/home" className="logo">
         M<span className="hidden">iralou</span>
       </Link>
+      <p className="paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      </p>
+      <div className="social-media">
+        <Link to="https://facebook.com" className="logo">
+          <ImFacebook />
+        </Link>
+        <Link to="https://instagram.com" className="logo">
+          <BsInstagram />
+        </Link>
+        <Link to="https://viemo.com" className="logo">
+          <BsVimeo />
+        </Link>
+        <Link to="https://twitter.com" className="logo">
+          <BsTwitter />
+        </Link>
+      </div>
       <p className="information">
         Information
         {footerNavigation.map((item, index) => {
