17a59f8921310852a90b94064cb91db14ce4819b
PJ Angeloni <pj.angeloni@gmail.com>
2023-07-06 16:45:14 -0400

    minor changes to footer before deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,53 +7,63 @@ import { ImFacebook } from "react-icons/im";
 import { BsInstagram, BsVimeo, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
-  const { footerNavigation, footerNavigation1 } = webSiteInfoDB;
+  const { footerNavigation, footerNavigation1, footerNavigation2 } =
+    webSiteInfoDB;
   return (
     <footer className="footer">
       <Link to="/home" className="logo">
         M<span className="hidden">iralou</span>
       </Link>
       <p className="paragraph">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem, ipsum
+        dolor sit amet consectetur adipisicing elit.
       </p>
       <div className="social-media">
-        <Link to="https://facebook.com" className="logo">
+        <Link to="https://facebook.com" className="logos">
           <ImFacebook />
         </Link>
-        <Link to="https://instagram.com" className="logo">
+        <Link to="https://instagram.com" className="logos">
           <BsInstagram />
         </Link>
-        <Link to="https://viemo.com" className="logo">
+        <Link to="https://viemo.com" className="logos">
           <BsVimeo />
         </Link>
-        <Link to="https://twitter.com" className="logo">
+        <Link to="https://twitter.com" className="logos">
           <BsTwitter />
         </Link>
       </div>
-      <p className="information">
-        Information
-        {footerNavigation.map((item, index) => {
-          return (
-            <NavItem
-              key={`menu-list-${index}`}
-              url={item.link}
-              title={item.title}
-            />
-          );
-        })}
-      </p>
-      <p className="quick-links">
-        Quick Links
-        {footerNavigation1.map((item, index) => {
-          return (
-            <NavItem
-              key={`menu-list-${index}`}
-              url={item.link}
-              title={item.title}
-            />
-          );
-        })}
-      </p>
+      <div className="info-links-contact">
+        <p className="information">
+          INFORMATION
+          {footerNavigation.map((item, index) => {
+            return (
+              <NavItem
+                key={`menu-list-${index}`}
+                url={item.link}
+                title={item.title}
+              />
+            );
+          })}
+        </p>
+        <p className="quick-links">
+          QUICK LINKS
+          {footerNavigation1.map((item, index) => {
+            return (
+              <NavItem
+                key={`menu-list-${index}`}
+                url={item.link}
+                title={item.title}
+              />
+            );
+          })}
+        </p>
+        <p className="contact-us">CONTACT US</p>
+        <p>
+          {footerNavigation2.address}
+          {footerNavigation2.phone}
+          {footerNavigation2.email}
+        </p>
+      </div>
       <hr></hr>
       <small className="copyright">COPYRIGHT © 2022 MIRALOU</small>
       <small className="rights">ALL RIGHTS RESERVED</small>
